47b05e668e56803223f1705d14b6979bf1c44303
User <User@192.168.1.125>
2022-10-28 16:26:29 +0200

    Début de l'exercice 3

---
+++ TP2_manip_BUATOIS/src/tp2_manip_buatois/TP2_manip_BUATOIS.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_buatois;
+
+/**
+ *
+ * @author User
+ */
+public class TP2_manip_BUATOIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+}
