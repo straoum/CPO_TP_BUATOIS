4814440d013bb533706efd482a7edfe040113b65
User <User@192.168.1.125>
2022-10-28 16:58:41 +0200

    2change de tartiflette 1 et tartiflette 2

--- TP2_manip_BUATOIS/src/tp2_manip_buatois/TP2_manip_BUATOIS.java
+++ TP2_manip_BUATOIS/src/tp2_manip_buatois/TP2_manip_BUATOIS.java
@@ -15,7 +15,21 @@ public class TP2_manip_BUATOIS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        Tartiflette assiette1 = new Tartiflette(500);
+        Tartiflette assiette2 = new Tartiflette(700);
+
+        Tartiflette assiette3 = assiette2;
+        assiette2 = assiette1;
+        assiette1 = assiette3;
+
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories);
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories);
+        
+         //   Moussaka assiette666 = assiette1 ;
+         //  Moussaka assiette667 = new Tartiflette() ; c'est incorrect car il est impossible de mélanger les classes Tartiflettes et Moussaka
+        
+        Moussaka [] Tab
+
     }
-    
-    
 }
