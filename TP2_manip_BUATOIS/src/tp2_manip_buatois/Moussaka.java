ba591853679ca5b06d7f1e668f98b6577d7ae5be
User <User@192.168.1.125>
2022-10-28 16:29:00 +0200

    création des classes Moussaka et Tartiflette

---
+++ TP2_manip_BUATOIS/src/tp2_manip_buatois/Moussaka.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_buatois;
+
+/**
+ *
+ * @author User
+ */
+public class Moussaka {
+    int nbCalories;
+    
+    public Moussaka(int cal){
+    nbCalories=cal;
+}
+}
