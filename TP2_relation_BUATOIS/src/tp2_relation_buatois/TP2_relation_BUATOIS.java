c77ac0800f902317120573a1c8208912732b5fd6
User <User@192.168.1.125>
2022-10-29 11:09:40 +0200

    Adaptation du code aux nouveaux objets

--- TP2_relation_BUATOIS/src/tp2_relation_buatois/TP2_relation_BUATOIS.java
+++ TP2_relation_BUATOIS/src/tp2_relation_buatois/TP2_relation_BUATOIS.java
@@ -15,6 +15,15 @@ public class TP2_relation_BUATOIS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
     
+    Voiture uneClio = new Voiture("Clio", "Renault", 5);
+    Voiture uneAutreClio = new Voiture("Clio", "Renault", 5);
+    Voiture une2008 = new Voiture("2008", "Peugeot", 6);
+    Voiture uneMicra = new Voiture("Micra", "Nissan", 4);
+    Personne bob = new Personne("Bobby", "Sixkiller");
+    Personne reno = new Personne("Reno", "Raines");
+
+    System.out.println ("liste des voitures disponibles :\n"+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+    
+}
 }
