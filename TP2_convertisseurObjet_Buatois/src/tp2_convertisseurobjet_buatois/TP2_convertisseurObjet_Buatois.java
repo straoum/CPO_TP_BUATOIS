fec00870a80b8a40f690f7d97970c4ea83740e4f
User <User@192.168.1.125>
2022-10-29 11:56:49 +0200

    Ecriture des commentaires

--- TP2_convertisseurObjet_Buatois/src/tp2_convertisseurobjet_buatois/TP2_convertisseurObjet_Buatois.java
+++ TP2_convertisseurObjet_Buatois/src/tp2_convertisseurobjet_buatois/TP2_convertisseurObjet_Buatois.java
@@ -16,75 +16,71 @@ public class TP2_convertisseurObjet_Buatois {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         Convertisseur convert1 = new Convertisseur(); //création d'un premier objet de type convertisseur
         Convertisseur convert2 = new Convertisseur(); //création d'un deuxième objet de type convertisseur
-        
+
         convert1.CelciusVersFarenheit(12); //exécution du convertisseur de degrés Celcius en Farenheit
         convert1.FarenheitVersCelcius(172); //exécution du convertisseur de degrés Farenheit en Celcius
         System.out.println(convert1);
-        
-        Scanner sc = new Scanner(System.in); 
+
+        Scanner sc = new Scanner(System.in); //demande à l'utilisateur quelle conversion il veut faire
         System.out.println("Quelles conversion voulez-vous faire: 1: Celsius-->Kelvin \n 2: Kelvin-->Celsius \n 3: Farenheit-->Celsius \n 4: Celsius-->Farenheit \n 5: Kelvin-->Farenheit \n 6: Farenheit-->Kelvin");
-        int a = sc.nextInt(); 
-        double temp=1.0;
-        
-        
-        if (a==1) {
-   
-           Convertisseur conversionchoisie = new Convertisseur();
-           conversionchoisie.CelciusVersKelvin(temp);
-           System.out.println(conversionchoisie);
-    
+        int a = sc.nextInt();
+        double temp = 1.0; //choix de la température par défaut à 1.0 unité
+
+        if (a == 1) { //choix d'une conversion de degrés Celsius vers Farenheit
+
+            Convertisseur conversionchoisie = new Convertisseur();
+            conversionchoisie.CelciusVersKelvin(temp);
+            System.out.println(conversionchoisie);
+
         }
-        if (a==2) {
-   
-           Convertisseur conversionchoisie = new Convertisseur();
-           conversionchoisie.CelciusVersKelvin(temp);
-           System.out.println(conversionchoisie);
-    
+        if (a == 2) { //choix d'une conversion de degrés Celsius vers Kelvin
+
+            Convertisseur conversionchoisie = new Convertisseur();
+            conversionchoisie.CelciusVersKelvin(temp);
+            System.out.println(conversionchoisie);
+
         }
-        if (a==3) {
-   
-           Convertisseur conversionchoisie = new Convertisseur();
-           conversionchoisie.KelvinVersCelcius(temp);
-           System.out.println(conversionchoisie);
+        if (a == 3) {
+
+            Convertisseur conversionchoisie = new Convertisseur();
+            conversionchoisie.KelvinVersCelcius(temp);
+            System.out.println(conversionchoisie);
         }
-    
-        if (a==4) {
-   
-           Convertisseur conversionchoisie = new Convertisseur();
-           conversionchoisie.FarenheitVersCelcius(temp);
-           System.out.println(conversionchoisie);
-    
-        }   
-        if (a==5) {
-   
-           Convertisseur conversionchoisie = new Convertisseur();
-           conversionchoisie.CelciusVersFarenheit(temp);
-           System.out.println(conversionchoisie);
-    
+
+        if (a == 4) {
+
+            Convertisseur conversionchoisie = new Convertisseur();
+            conversionchoisie.FarenheitVersCelcius(temp);
+            System.out.println(conversionchoisie);
+
         }
-        
-      
-        if (a==6) {
-   
-           Convertisseur conversionchoisie = new Convertisseur();
-           conversionchoisie.KelvinVersFarenheit(temp);
-           System.out.println(conversionchoisie);
-    
-        } 
-        
-        if (a==6) {
-   
-           Convertisseur conversionchoisie = new Convertisseur();
-           conversionchoisie.FarenheitVersKelvin(temp);
-           System.out.println(conversionchoisie);
-    
-        } 
-        
+        if (a == 5) {
+
+            Convertisseur conversionchoisie = new Convertisseur();
+            conversionchoisie.CelciusVersFarenheit(temp);
+            System.out.println(conversionchoisie);
+
+        }
+
+        if (a == 6) {
+
+            Convertisseur conversionchoisie = new Convertisseur();
+            conversionchoisie.KelvinVersFarenheit(temp);
+            System.out.println(conversionchoisie);
+
+        }
+
+        if (a == 6) {
+
+            Convertisseur conversionchoisie = new Convertisseur();
+            conversionchoisie.FarenheitVersKelvin(temp);
+            System.out.println(conversionchoisie);
+
+        }
+
     }
-    
-   
-    
+
 }
