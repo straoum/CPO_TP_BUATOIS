e1d90b3de2f25eb580b0cdb7428ba6459b6d20b0
User <User@192.168.1.125>
2022-10-29 12:16:00 +0200

    Ecriture des commentaires

--- TP2_Bieres_Buatois/src/tp2_bieres_buatois/TP2_Bieres_Buatois.java
+++ TP2_Bieres_Buatois/src/tp2_bieres_buatois/TP2_Bieres_Buatois.java
@@ -15,23 +15,23 @@ public class TP2_Bieres_Buatois {
      */
     public static void main(String[] args) { 
         // TODO code application logic here
-        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6,
-                 "Abbaye de Leffe");
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6,    //création de nouvelles bières
+                 "Abbaye de Leffe"); 
         BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",
                 7.0, "Dubuisson");
         BouteilleBiere autre1Biere = new BouteilleBiere("Chouffe", 8.0, "Achouffe");
         BouteilleBiere autre2Biere = new BouteilleBiere("Pelforth", 5.8, "Heineken");
         BouteilleBiere autre3Biere = new BouteilleBiere("Bellion", 5.5, "Bellion Boisson");
 
-        autre1Biere.lireEtiquette();
+        autre1Biere.lireEtiquette(); //lecture de l'étiquette des différentes bières
         uneBiere.lireEtiquette();
         autreBiere.lireEtiquette();
         autre2Biere.lireEtiquette();
         autre3Biere.lireEtiquette();
 
-        autre1Biere.Décapsuler();
+        autre1Biere.Décapsuler();  //décapsulage de ces bières
         autre2Biere.Décapsuler();
-        System.out.println(uneBiere) ;
+        System.out.println(uneBiere) ;  //on demande si ces bières sont déjà ouvertes
         System.out.println(autre1Biere) ;
         System.out.println(autre2Biere) ;
 
