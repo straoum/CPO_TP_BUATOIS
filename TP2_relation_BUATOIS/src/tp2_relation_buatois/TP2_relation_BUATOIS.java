67d279f2925380a19c20e5d205dd90cb14c51c40
User <User@192.168.1.125>
2022-10-30 21:57:35 +0100

    Fin du TP2

--- TP2_relation_BUATOIS/src/tp2_relation_buatois/TP2_relation_BUATOIS.java
+++ TP2_relation_BUATOIS/src/tp2_relation_buatois/TP2_relation_BUATOIS.java
@@ -26,26 +26,34 @@ public class TP2_relation_BUATOIS {
         bob.liste_voitures[0] = uneClio; //attribution d'une clio à Bob comme première voiture
         bob.nbVoitures = 1;
         uneClio.proprietaire = bob;
-        
+
         bob.liste_voitures[1] = une2008;//atribution d'une 2008 à Bob comme deuxième voiture
         bob.nbVoitures = 1;
         une2008.proprietaire = bob;
-        
+
         reno.liste_voitures[0] = uneAutreClio;
         reno.nbVoitures = 1;
         uneAutreClio.proprietaire = reno;
-        
+
         reno.liste_voitures[1] = uneMicra;
         reno.nbVoitures = 1;
         uneMicra.proprietaire = reno;
 
         System.out.println("liste des voitures disponibles :\n" + uneClio + "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra); //affichage des voitures dispos
-        System.out.println("la premiere voiture de Bob est " +bob.liste_voitures[0] ) ; //affichage des voitures de Bob
-        System.out.println("la deuxième voiture de Bob est " +bob.liste_voitures[1] ) ;
-        
-        System.out.println("la premiere voiture de Reno est " + reno.liste_voitures[0] ) ; // affchage des voitures de Reno
-        System.out.println("la deuxième voiture de Reno est " +reno.liste_voitures[1] ) ;
-  
+        System.out.println("la premiere voiture de Bob est " + bob.liste_voitures[0]); //affichage des voitures de Bob
+        System.out.println("la deuxième voiture de Bob est " + bob.liste_voitures[1]);
 
+        System.out.println("la premiere voiture de Reno est " + reno.liste_voitures[0]); // affchage des voitures de Reno
+        System.out.println("la deuxième voiture de Reno est " + reno.liste_voitures[1]);
+
+    
+
+    System.out.println (bob.toString() + reno.toString());
+        
+        Voiture voit1 = new Voiture("mod1", "mar1", 10);
+
+    reno.ajouter_voiture (voit1);
+
+    System.out.println (reno.toString() + voit1.toString());
     }
 }
