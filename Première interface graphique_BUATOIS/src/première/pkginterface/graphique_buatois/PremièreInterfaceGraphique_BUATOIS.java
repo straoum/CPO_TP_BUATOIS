41c5daa7154eacb72c7a9ba87c71151a2fb73a9e
User <User@192.168.1.125>
2022-11-07 12:35:17 +0100

    Création d'un nouveau projet graphique en roue libre

--- Première interface graphique_BUATOIS/src/première/pkginterface/graphique_buatois/PremièreInterfaceGraphique_BUATOIS.java
+++ Première interface graphique_BUATOIS/src/première/pkginterface/graphique_buatois/PremièreInterfaceGraphique_BUATOIS.java
@@ -13,8 +13,6 @@ public class PremièreInterfaceGraphique_BUATOIS {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   
     
 }
