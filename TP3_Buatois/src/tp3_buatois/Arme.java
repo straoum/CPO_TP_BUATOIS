da92520c29bfa56dab1c18cac5e7466aefe1fd58
User <User@192.168.1.125>
2022-11-14 09:34:49 +0100

    Création du projet TP3 héritage

---
+++ TP3_Buatois/src/tp3_buatois/Arme.java
@@ -0,0 +1,14 @@
+package tp3_buatois;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Arme {
+    
+}
