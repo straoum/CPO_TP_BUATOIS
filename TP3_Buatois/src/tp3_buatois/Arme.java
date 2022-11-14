3d073f230ae094ce15f6d3e3b65605528489406c
User <User@192.168.1.125>
2022-11-14 11:11:35 +0100

    Création des 4 objets

--- TP3_Buatois/src/tp3_buatois/Arme.java
+++ TP3_Buatois/src/tp3_buatois/Arme.java
@@ -1,14 +1,47 @@
-package tp3_buatois;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package tp3_buatois;
 
 /**
  *
  * @author User
  */
 public class Arme {
+        private int attaque;
+        String nom;
+    public Arme(String n, int i){
+        nom = n;
+          
+}
+
+    public Arme(int attaque, String nom) {
+        this.attaque = attaque;
+        this.nom = nom;
+    }
+
+    public int getAttaque() {
+        return attaque;
+    }
+
+    public void setAttaque(int attaque) {
+        if (attaque>0) {
+            attaque = attaque;   
+        }
+        else{
+            attaque = 1;
+        }
+        if (attaque>100) {
+            attaque = 100;
+        }
+        this.attaque = attaque;
+    }
+
+    @Override
+    public String toString() {
+        return "Arme{" + "attaque=" + attaque + ", nom=" + nom + '}';
+    }
+
     
 }
--- TP3_Buatois/src/tp3_buatois/TP3_Buatois.java
+++ TP3_Buatois/src/tp3_buatois/TP3_Buatois.java
@@ -15,6 +15,18 @@ public class TP3_Buatois {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Arme m = new Arme("Arme Standard",40);
+        Bâton baton1 = new Bâton(4,"Chêne",5);
+        Epée epee1 = new Epée(7, "Excalibur",5);
+        Bâton baton2 = new Bâton(5,"Charme",6);
+        Epée epee2 = new Epee(4,"Durandal",7);
+       
+        
+         
+        
+
+        
+        
     }
     
 }
