2341f2ca47b7e3af0cd0b21176fbd43a7abb9d55
User <User@192.168.1.125>
2022-11-15 15:23:37 +0100

    Création du tableau dynamique

--- TP3_Buatois/src/tp3_buatois/TP3_Buatois.java
+++ TP3_Buatois/src/tp3_buatois/TP3_Buatois.java
@@ -27,7 +27,7 @@ public class TP3_Buatois {
         Bâton baton2 = new Bâton(5,"Charme",6);
         Epée epee2 = new Epée(4,"Durandal",7);
         
-        ArrayList<Arme> tabArme = new ArrayList<Arme>();
+        ArrayList<Arme> tabArme = new ArrayList<>(); // création d'un tableau dynamique stockant les armes
         
         tabArme.add(baton1);
         tabArme.add(baton2);
@@ -36,9 +36,27 @@ public class TP3_Buatois {
         
         int a = tabArme.size();
         for (int i=0;i<a;i++){
-            System.out.println("voici les caractéristiques de l'arme :"+tabArme.get(i));
+            System.out.println("voici les caractéristiques de l'arme :"+tabArme.get(i)); //affichage des caractéristiques des armes
         }
         
-    }
+    Magicien magicien1 = new Magicien(true,65,"Gandalf"); //création de 2 guerriers et de 2 magiciens
+    Guerrier guerrier1 = new Guerrier(false,78,"Conan");
+    Magicien magicien2 = new Magicien(false,44,"Garcimore");
+    Guerrier guerrier2 = new Guerrier(true,45,"Lannister");
+        
+    ArrayList<Personnage> tabpersonnage = new ArrayList<>(); // création d'un tableau dynamique stockant les personnages
+        
+    tabpersonnage.add(magicien1);
+    tabpersonnage.add(magicien2);
+    tabpersonnage.add(guerrier1);
+    tabpersonnage.add(guerrier2);
     
+    int z = tabArme.size(); //affichage des informations sur les personnages
+        for (int i=0;i<z;i++){
+            System.out.println("voici les informations sur le personnage :"+tabpersonnage.get(i));
+        }
+    
+    }
+   
+
 }
