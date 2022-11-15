a8fe3a9532a4cfcc1f018fc2ee494a217c5d74c2
User <User@192.168.1.125>
2022-11-15 16:46:00 +0100

    Création des nouveaux objets

--- TP3_Buatois/src/tp3_buatois/TP3_Buatois.java
+++ TP3_Buatois/src/tp3_buatois/TP3_Buatois.java
@@ -24,7 +24,7 @@ public class TP3_Buatois {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Arme m = new Arme("Arme Standard",40);
+        Arme m = new Arme("Arme Standard",40); 
         Bâton baton1 = new Bâton(4,"Chêne",5); //création de 2 bâtons et 2 épées
         Epée epee1 = new Epée(7, "Excalibur",5);
         Bâton baton2 = new Bâton(5,"Charme",6);
@@ -32,7 +32,7 @@ public class TP3_Buatois {
         
         ArrayList<Arme> tabArme = new ArrayList<>(); // création d'un tableau dynamique stockant les armes
         
-        tabArme.add(baton1);
+        tabArme.add(baton1); //ajout des armes dans le tableau
         tabArme.add(baton2);
         tabArme.add(epee1);
         tabArme.add(epee2);
@@ -49,7 +49,7 @@ public class TP3_Buatois {
         
     ArrayList<Personnage> tabpersonnage = new ArrayList<>(); // création d'un tableau dynamique stockant les personnages
         
-    tabpersonnage.add(magicien1);
+    tabpersonnage.add(magicien1); //ajout des personnages dans le tableau
     tabpersonnage.add(magicien2);
     tabpersonnage.add(guerrier1);
     tabpersonnage.add(guerrier2);
@@ -59,6 +59,14 @@ public class TP3_Buatois {
             System.out.println("voici les informations sur le personnage :"+tabpersonnage.get(i));
         }
     
+    Magicien magicien3 = new Magicien(true,88,"Perlimpimpim"); //création de 1 magicien, 1 guerrier, 3 bâtons et 3 épées
+    Guerrier guerrier3 = new Guerrier(true,98,"Rambo");
+    Bâton baton3 = new Bâton(4,"Cèdre",6);
+    Bâton baton4 = new Bâton(5,"Olivier",7);
+    Bâton baton5 = new Bâton(2,"Charmillle",3);
+    Epée epee3 = new Epée(7, "Gertrude",5);
+    Epée epee4 = new Epée(7, "Charles-Henri",5);
+    Epée epee5 = new Epée(7, "Jo",5);
     }
    
 
