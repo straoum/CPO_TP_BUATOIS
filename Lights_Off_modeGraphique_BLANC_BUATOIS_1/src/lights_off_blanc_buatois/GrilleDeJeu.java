59bb27112ed6969ff188b907c91227f8965d69c7
User <User@192.168.1.125>
2022-12-14 12:32:29 +0100

    creation partie graphique

---
+++ Lights_Off_modeGraphique_BLANC_BUATOIS_1/src/lights_off_blanc_buatois/GrilleDeJeu.java
@@ -0,0 +1,194 @@
+package lights_off_blanc_buatois;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class GrilleDeJeu {
+    
+    Cellules_Lumineuses[][] grille = new Cellules_Lumineuses[5][5];
+    String lumiereCourant;
+    
+    public GrilleDeJeu(){
+        for (int l=0;l<5;l++){
+            for (int c=0;c<5;c++){
+                grille[l][c]=new Cellules_Lumineuses();
+                if (randomlumière()==true){
+                    eteindreLumiere(l,c);
+                }
+            }
+        }
+    }
+    
+    public boolean randomlumière(){
+        if (Math.random()>0.5){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean presenceLumiere(int l, int c){
+       if (grille[l][c].presenceLumiere()==true){
+           return true;
+       }else{
+           return false;
+       }
+    }
+    
+    public void allumerLumiere(int l, int c){
+        if (this.presenceLumiere(l, c)==false){
+            grille[l][c].allumerLumiere();
+        }
+    }
+    
+    public void eteindreLumiere(int l, int c){
+        if (this.presenceLumiere(l, c)==true){
+            grille[l][c].eteindreLumiere();
+        }
+    }
+    
+    public void clicutilisateur(int l,int c){
+        if (presenceLumiere(l, c)==false){ //allume ou eteint la case cliqué
+            allumerLumiere(l, c);
+        }else{
+            eteindreLumiere(l, c);   
+        }
+        if (l==0 || l==4){
+            if (c==0 || c==4){
+                if (l==0 && (c==0 || c==4)){ // ligne du bas
+                    if (presenceLumiere(l+1, c)==false){
+                        allumerLumiere(l+1, c);
+                    }else{
+                        eteindreLumiere(l+1, c);   
+                    }
+                    if (c==4){ // coins bas droit
+                        if (presenceLumiere(l, c-1)==false){
+                            allumerLumiere(l, c-1);
+                        }else{
+                            eteindreLumiere(l, c-1);   
+                        }
+                    }else{ // coins bas gauche
+                        if (presenceLumiere(l, c+1)==false){
+                            allumerLumiere(l, c+1);
+                        }else{
+                            eteindreLumiere(l, c+1);   
+                        }
+                    }
+                }else if (l==4 && (c==0 || c==4)){ // ligne du haut
+                    if (presenceLumiere(l-1, c)==false){
+                        allumerLumiere(l-1, c);
+                    }else{
+                        eteindreLumiere(l-1, c);   
+                    }
+                    if (c==4){ // coins haut droit
+                        if (presenceLumiere(l, c-1)==false){
+                            allumerLumiere(l, c-1);
+                        }else{
+                            eteindreLumiere(l, c-1);   
+                        }
+                    }else{ // coins haut gauche
+                        if (presenceLumiere(l, c+1)==false){
+                            allumerLumiere(l, c+1);
+                        }else{
+                            eteindreLumiere(l, c+1);   
+                        }
+                    }
+                } 
+            }else{
+                if (presenceLumiere(l, c+1)==false){
+                    allumerLumiere(l, c+1);
+                }else{
+                    eteindreLumiere(l, c+1);   
+                }             
+                if (presenceLumiere(l, c-1)==false){
+                    allumerLumiere(l, c-1);
+                }else{
+                    eteindreLumiere(l, c-1);   
+                }                
+                if (l==0){ // juste ligne du bas sans coins
+                    if (presenceLumiere(l+1, c)==false){
+                        allumerLumiere(l+1, c);
+                    }else{
+                        eteindreLumiere(l+1, c);   
+                    }
+                }
+                if (l==4){ // juste ligne du haut sans coins
+                    if (presenceLumiere(l-1, c)==false){
+                        allumerLumiere(l-1, c);
+                    }else{
+                        eteindreLumiere(l-1, c);   
+                    }
+                }
+            }
+        }else{ 
+            if (presenceLumiere(l+1, c)==false){
+                allumerLumiere(l+1, c);
+            }else{
+                eteindreLumiere(l+1, c);   
+            }
+            if (presenceLumiere(l-1, c)==false){
+                allumerLumiere(l-1, c);
+            }else{
+                eteindreLumiere(l-1, c);   
+            }
+            if (c==0){ // pour les colones de droite 
+                if (presenceLumiere(l, c+1)==false){
+                    allumerLumiere(l, c+1);
+                }else{
+                    eteindreLumiere(l, c+1);   
+                }
+            }else if(c==4){// pour les colones de gauche
+                if (presenceLumiere(l, c-1)==false){
+                    allumerLumiere(l, c-1);
+                }else{
+                    eteindreLumiere(l, c-1);   
+                }
+            }else { // pour toutes les cases du centre
+                if (presenceLumiere(l, c+1)==false){
+                    allumerLumiere(l, c+1);
+                }else{
+                    eteindreLumiere(l, c+1);   
+                }
+                if (presenceLumiere(l, c-1)==false){
+                    allumerLumiere(l, c-1);
+                }else{
+                    eteindreLumiere(l, c-1);   
+                }
+            }
+        }
+    }
+    
+    public boolean gagner(){
+        int a=0;
+        for (int l=0; l<5;l++){
+            for (int c=0;c<5;c++){
+                if (presenceLumiere(l, c)==false){
+                    a=a+1;
+                }
+            }
+        }
+        if (a==25){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public void affichergrilledansconsole(){
+        for (int l=4;l>-1;l--){
+            for (int c=0;c<5;c++){
+                System.out.print("["+grille[l][c].lumiereCourant+"]");
+                if ( c== 4){
+                    System.out.print("\n");
+                }
+            }
+        }
+    }
+
+}
---
+++ Lights_Off_modeGraphique_BLANC_BUATOIS_1/src/lights_off_blanc_buatois/Partie.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lights_off_blanc_buatois;
+
+import java.util.Scanner;
+/**
+ *
+ * @author User
+ */
+public class Partie {
+    
+    
+    Joueur JoueurCourant;
+    GrilleDeJeu Grille;
+  
+    public void initialisation_partie(){
+        Grille = new GrilleDeJeu();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer votre nom :");
+        //JoueurCourant.nom = sc.nextLine();        
+        JoueurCourant = new Joueur(sc.nextLine());
+        //Grille.affichergrilledansconsole();
+    }
+    
+    public void partie(){
+        Scanner rep = new Scanner(System.in);
+        System.out.println("1)Niveau 1 avec coup illimité  2)Niveau 2 coup restraint à 50");
+        int niveau=rep.nextInt();
+        int cpt=50;
+        Grille.affichergrilledansconsole();
+        if (niveau==1){
+            while (Grille.gagner()==false){
+                int l=100;
+                int c=100;
+                Scanner repl = new Scanner(System.in);
+                Scanner repc = new Scanner(System.in);
+                while (l<0 || l>6 ){
+                    System.out.println(" taper un nombre entre 1 et 5 compris pour la ligne");
+                    l = repl.nextInt();
+                }
+                while( c<0 || c>6 ){
+                    System.out.println(" taper un nombre entre 1 et 5 compris pour la colonne");
+                    c = repc.nextInt();
+                }
+                Grille.clicutilisateur(l-1,c-1);
+                Grille.affichergrilledansconsole();
+            }
+            System.out.println(" vous avez gagné");
+        }else{
+            while (Grille.gagner()==false || cpt==0 ){
+                int l=100;
+                int c=100;
+                Scanner repl = new Scanner(System.in);
+                Scanner repc = new Scanner(System.in);
+                while (l<0 || l>6 ){
+                    System.out.println(" taper un nombre entre 1 et 5 compris pour la ligne");
+                    l = repl.nextInt();
+                }
+                while( c<0 || c>6 ){
+                    System.out.println(" taper un nombre entre 1 et 5 compris pour la colonne");
+                    c = repc.nextInt();
+                }
+                Grille.clicutilisateur(l-1,c-1);
+                Grille.affichergrilledansconsole();
+                cpt=cpt-1;
+                System.out.println("il vous reste "+cpt+" coup.");
+            }
+            if (cpt==0){
+                System.out.println(" vous avez perdu");
+            }else{
+                cpt=50-cpt;
+                System.out.println("vous avez gagné en "+cpt+" coup" );
+            }
+        }
+    }
+}
