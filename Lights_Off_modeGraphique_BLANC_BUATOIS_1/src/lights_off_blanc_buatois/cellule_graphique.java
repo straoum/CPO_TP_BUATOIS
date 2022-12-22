18fddbf8a245c5fb162f39d0c647882d821435d7
User <User@192.168.1.125>
2022-12-22 17:17:57 +0100

    Modification de cellule graphique (on n'importe plus des images à afficher sur les boutons mais on affiche la couleur de la cellure grise pour "éteint" et jaune pour "allumé")

--- Lights_Off_modeGraphique_BLANC_BUATOIS_1/src/lights_off_blanc_buatois/cellule_graphique.java
+++ Lights_Off_modeGraphique_BLANC_BUATOIS_1/src/lights_off_blanc_buatois/cellule_graphique.java
@@ -4,29 +4,28 @@
  */
 package lights_off_blanc_buatois;
 
+import java.awt.Color;
 import java.awt.Graphics;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
+
 /**
  *
  * @author User
  */
 public class cellule_graphique extends JButton{
     Cellules_Lumineuses celluleassocié;
-    ImageIcon img_allume = new javax.swing.ImageIcon(getClass().getResource("/image/allumé.png"));
-    ImageIcon img_eteint = new javax.swing.ImageIcon(getClass().getResource("/image/eteint.png"));
     
     public cellule_graphique(Cellules_Lumineuses unecellule){
         celluleassocié=unecellule;
     }
-
-    @Override
-    public void paintComponent(Graphics G) {
+    
+    public void paintComponent(Graphics G){
         super.paintComponent(G);
-        if (celluleassocié.presenceLumiere() == true) {
-            setIcon(img_eteint);
-        } else {
-            setIcon(img_allume);
-        } 
+        if(celluleassocié.presenceLumiere()){
+            setBackground(Color.YELLOW);
+        }else{
+            setBackground(Color.gray);
+        }
     }
+    
 }
